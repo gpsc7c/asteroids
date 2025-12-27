8112b92b206197792c16f21e3d2da41e0889ba13
gpsc7c <gpsc7c@umsystem.edu>
2025-12-27 19:24:33 +0000

    Opens a window, doesn't stay open

---
+++ constants.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+#!/usr/bin/env python3
+
+import pygame
+import constants
+from logger import log_state
+
+
 def main():
+    pygame.init()
     print(f"Starting Asteroids with pygame version: VERSION{pygame.version.ver}")
+    #print(f"Screen width: {constants.SCREEN_WIDTH}")
+    #print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while():
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+        
 
 
 if __name__ == "__main__":
