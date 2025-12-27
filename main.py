e19ae51acee5ad9ff55fbf130d473f6c115ba9c7
gpsc7c <gpsc7c@umsystem.edu>
2025-12-27 19:28:39 +0000

    Opens, stays open, and has a quit button

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ def main():
     #print(f"Screen width: {constants.SCREEN_WIDTH}")
     #print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    while():
+    while(True):
         log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+            return
         screen.fill("black")
         pygame.display.flip()
         
