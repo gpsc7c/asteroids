7ab77c891f2c47c73960e748ebc0cdcdac3f5fcb
gpsc7c <gpsc7c@umsystem.edu>
2025-12-28 18:19:56 +0000

    locked to 60 fps

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ def main():
     fps_clock = pygame.time.Clock()
     dt = 0
     print(f"Starting Asteroids with pygame version: VERSION{pygame.version.ver}")
-    #print(f"Screen width: {constants.SCREEN_WIDTH}")
-    #print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, 
+                                      constants.SCREEN_HEIGHT))
     while(True):
+        #calls log
         log_state()
+        #shuts screen down if exit clicked
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #fills and updates screen
         screen.fill("black")
         pygame.display.flip()
+        #updates the screen + updates delta value (since last frame was drawn)
+        dt = (fps_clock.tick(60)/1000)
+        #print(dt)
         
 
 
